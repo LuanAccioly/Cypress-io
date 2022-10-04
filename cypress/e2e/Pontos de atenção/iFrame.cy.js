47b790c80549fbed84d992702157e2332804cae0
Luan Accioly <acciolyluan@gmail.com>
2022-10-04 18:30:03 -0300

    Update iFrame.cy.js

--- cypress/e2e/Pontos de atenção/iFrame.cy.js
+++ cypress/e2e/Pontos de atenção/iFrame.cy.js
@@ -6,10 +6,12 @@ describe('iFrames', () => {
         cy.get('#frame1').then(iframe => {
             const body = iframe.contents().find('body')
             cy.wrap(body).find('#tfield').type('digitando em um iframe')
-            cy.get('#alert').click()
-        
-
-
         })
+    })
+    
+    it('Teste diretamente do iFrame', () => {
+        cy.visit('https://wcaquino.me/cypress/frame.html')
+        cy.get('#otherButton').click();
+        
     })
 })
