29435e4f123ba90313eb9963bfbd251f22e93990
Luan Accioly <acciolyluan@gmail.com>
2022-10-05 00:52:28 -0300

    Update iFrame.cy.js

--- cypress/e2e/Pontos de atenção/iFrame.cy.js
+++ cypress/e2e/Pontos de atenção/iFrame.cy.js
@@ -1,7 +1,7 @@
-/// <reference types="cypress" />
 
-describe('iFrames', () => {
-    it('Tentando digitar em um textBox dentro de um iFrame', () => {
+
+describe.skip('iFrames', () => {
+    it('Digitando no iFrame', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html')
         cy.get('#frame1').then(iframe => {
             const body = iframe.contents().find('body')
@@ -13,5 +13,22 @@ describe('iFrames', () => {
         cy.visit('https://wcaquino.me/cypress/frame.html')
         cy.get('#otherButton').click();
         
+    })
+})
+
+describe('Plugin iFrame', () => {
+
+    it('Digitando no iFrame', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.frameLoaded('#frame1')
+        /* cy.iframe().find('#otherButton').click(); */
+        cy.iframe().find('#tfield').type('digitando em um iframe')
+        cy.iframe().find('#tfield').should('have.value','digitando em um iframe')
+    })
+
+    it('Alert', () => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.frameLoaded('#frame1')
+        cy.iframe().find('#otherButton').click();
     })
 })
