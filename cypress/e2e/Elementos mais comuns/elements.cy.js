6ad2265d1aa82fa09809f801137c6b72c3c01e59
Luan Accioly <acciolyluan@gmail.com>
2022-10-10 00:07:25 -0300

    Update elements.cy.js

--- cypress/e2e/Elementos mais comuns/elements.cy.js
+++ cypress/e2e/Elementos mais comuns/elements.cy.js
@@ -1,9 +1,21 @@
 /// <reference types="cypress" />
 
+before(() => {
+    cy.visit('https://wcaquino.me/cypress/componentes.html')
+})
+
 describe("trabalhando com elementos básicos", () => {
-    it("textos", () => {
+    it.skip("textos", () => {
         cy.visit('https://sigs.ufrpe.br/sigaa/verTelaLogin.do')
         cy.get('body').should('contain', 'sistema')
         cy.get('[style="margin: 0 auto; width: 645px; "] > div > center').should('contain', 'Perdeu')      
+    })
+
+    it('links', () => {
+        cy.get('[href="#"]').click()
+        cy.get('#resultado').should('have.text', 'Voltou!')
+        cy.reload()
+        cy.get('#resultado').should('have.not.text', 'Voltou!')
+        
     })
 }) 
